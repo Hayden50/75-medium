29f6c6ce43b36d6208dfc0354d51a727f74177c1
Hayden50 <haydenrieder50@hotmail.com>
2023-01-04 23:13:56 -0500

    Update user routes

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import {z} from 'zod';
 import prisma from '../index';
+import {Prisma} from '@prisma/client'
 
 const userRouter = express.Router();
 userRouter.use(express.json());
@@ -11,25 +12,41 @@ const CreateUserSchema = z.object({
 })
 
 userRouter.get('/', async (_, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.send(JSON.stringify(users));
+  try {
+    const users = await prisma.user.findMany();
+    res.send(JSON.stringify(users));
+  } catch (e) {
+    errorHandle(e, res);
+  }
 });
+
+userRouter.get('/:id', async(req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const user = await prisma.user.findUnique({
+      where: { id: id }
+    })
+    res.send(JSON.stringify(user));
+  } catch (e) {
+    errorHandle(e, res);
+  }
+})
 
 userRouter.post('/create-user', async (req: Request, res: Response) => {
   try {
     res.send(CreateUserSchema.parse(req.body));
   } catch (e) {
-    const error = e as Error;
-    res.send(error.message);
+    errorHandle(e, res);
   }
 });
 
-// const errorHandle = (e: unknown, res: Response) => {
-//   if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002') {
-//     res.send('There is a unique constraint violation, a new user cannot be created with this email'); 
-//   } else {
-//     res.send('There was an unexpected error');
-//   }
-// } 
+const errorHandle = (e: unknown, res: Response) => {
+  if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002') {
+    res.send('There is a unique constraint violation, a new user cannot be created with this email'); 
+  } else {
+    const error = e as Error;
+    res.send(error.message);
+  }
+} 
 
 export default userRouter;
